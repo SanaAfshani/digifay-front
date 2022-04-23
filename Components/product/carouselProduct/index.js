d74cddcc22434988297aef26fa1c94c1fa179aa9
front.synapes <front.synapes@gmail.com>
2022-04-23 12:53:31 +0430

    fix FontFamily

--- Components/product/carouselProduct/index.js
+++ Components/product/carouselProduct/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const CarouselProduct = () => {
     return (
-        <>
-            
-        </>
+        <div className="d-flex flex-column flex-md-row ">
+            <div>
+                20
+            </div>
+            <div>
+                80
+            </div>
+        </div>
     );
 };
 
