149624bdabf65020fd7f5b4884caa6d2a4e1269d
front.synapes <front.synapes@gmail.com>
2022-04-24 01:39:28 +0430

    asd

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import SingleProduct from '../Components/product';
 import {useSelector} from "react-redux";
 import axios from "axios";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 //
-// export async function getServerSideProps(Lang) {
-//     let res = await axios.get("https://1d961432-e87f-44ca-8123-78c940f8e2bc.mock.pstmn.io/Product/1156")
-//     return {
-//         props: {data: res.data}
-//     }
-// }
+export async function getServerSideProps(Lang) {
+    let res = await axios.get("https://749ad6cc-6129-4c5d-b84a-3d7592dc87cd.mock.pstmn.io/GetProuct/1156")
+    return {
+        props: {data:res.data.entity}
+    }
+}
 
 
   export default function Home({data}) {
+
     const state = useSelector(state => state.toolsReducer)
+      console.log(data)
     return (
         <>
             <SingleProduct/>
